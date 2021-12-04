a42dfbbf6c4ca0fa79eb436d3b1dc487628b990a
albaygo <albayg88@hotmail.com>
2021-12-04 14:58:13 -0600

    Se agregan los archivos de register y login de Yinavis

---
+++ app/Views/Layouts/login.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>SIGMED</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="login.css" media="screen">
+    <style>
+
+html, body {
+  height: 100%;
+  margin: 0; }
+
+.full-height {
+  height: 100%; }
+
+</style>
+</head>
+<body>
+
+<section id="A">
+  </p></p><h1> Bienvenido a SIGMED </p></p></h1>
+  <p> La salud es la mayor posesión. La alegría es el mayor tesoro.</p>
+  </section> 
+
+  <section id="B">
+  </p></p><h1> INICIO DE SESIÓN</h1></p></p></h1>
+  </section> 
+
+  <section id="C">
+        <div>
+        </p></p>
+          <label for="exampleInputEmail1" class="form-label">Correo Electrónico</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"> 
+          <p></p>  <p></p>
+        <div>
+        <div class="contra">
+          <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+          <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+   </section>   
+    
+<section id="D">
+        <h7> ¿Olvidó su contraseña?</h7> <p></p> <style> h7 {  text-decoration-line: underline; }    </style>
+        <button type="submit" class="btn btn-primary">Iniciar Sesión</button></p></p>
+    </section>
+
+
+<section id="E">
+            <h7> Cree una cuenta nueva</h7><p></p> 
+          <button type="reg" class="btn btn-primary">Registrarse</button>
+    </section>
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ app/Views/Layouts/register.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>SIGMED</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="register.css" media="screen">
+    <style>
+
+html, body {
+  height: 100%;
+  margin: 0; }
+
+.full-height {
+  height: 100%;}
+
+</style>
+</head>
+<body>
+
+
+<section id="A">
+    <h1> Registro de Usuario </p></p></h1>
+    <p> ALGO CREATIVO AQUI</p>
+  </section> 
+
+<section id="B">
+    <h2> Ingrese los datos para registarse a SIGMED </h2></p></p></h1>
+  </section> 
+
+<section id="C">
+  <form>
+          <div class="cedula">
+            <label for="IDdeUsuario" class="form-label">No° de Cédula/Pasaporte</label>
+            <input type="cedula" class="form-control" id="IDdeUsuario" aria-describedby="idUsuario">  
+          </div>
+          <div class="nombre">
+            <label for="Nom" class="form-label">Nombre</label>
+            <input type="nombre" class="form-control" id="Nom" aria-describedby="nomUsuario">  
+          </div>
+          <div class="apellido">
+            <label for="Apel" class="form-label">Apellido</label>
+            <input type="apell" class="form-control" id="Apel" aria-describedby="apellUsuario">  
+          </div>
+          <div class="telefono">
+            <label for="Tel" class="form-label">Telefono</label>
+            <input type="telf" class="form-control" id="Tel" aria-describedby="telUsuario">  
+          </div>
+          <div class="corre">
+            <label for="exampleInputEmail1" class="form-label">Correo Electrónico</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">  
+          </div>
+          <div class="contra">
+            <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+          </div><p></p>
+    </form>
+</section>
+
+<section id="D">
+    <button type="submit" class="btn btn-primary">Registrarse</button><p></p>
+    <h7> ¿Ya tiene cuenta? Inicie sesión</h7>
+    <style> h7 {  text-decoration-line: underline; }    </style>
+</section>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
