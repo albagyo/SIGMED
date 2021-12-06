e3078e677a7dd4678f42738a7b14ae2095775cc4
Isaias <isaias_abel.23@outlook.com>
2021-12-05 19:26:31 -0500

    Pagina de opciones al 50%

--- app/Views/Layouts/layout.php
+++ app/Views/Layouts/layout.php
@@ -27,52 +27,8 @@ html, body {
 	</header>
   
     
-<!--codigo de opciones.php '''''NO VA AQUÍ''''''''-->
-    
-<div class="row align-items-start" style=" height:100%">
-        <div class="col-md-3 ml-sm-12 col-lg-3 col-xl-2 " style="height:100%; background: #B9FFFF; ">
-            <div style="display: table; width:100%; height:8%;" >
-                <div  style="display: table-cell; background: #033FAC; width:100%; height:100%; color: #ffffff; text-align:center; vertical-align:middle">
-                    <h2 >Agendar Cita</h2>
-                </div>
-            </div>
-            <main role="main" class="col-md-11 ml-sm-11 col-lg-11 px-1">
-            <form>
-                <div class="mb-3" style="padding: 5% 0%">
-                    <label for="SeleccionePoliclinica" class="form-label">Seleccione una Policlínica</label>
-                    <!--<select class="form-control input-lg" id="nuevaCategoria" name="nuevaCategoria" required></select>-->
-                    <select id="SeleccionePoliclinica" class="form-select" name="SeleccionePoliclinica" required>  
-                        <option value="">Disabled select</option>               
-                        
-              </select>
-              </div>                 
-                <div class="mb-3" style="padding: 5% 0%">
-                    <label for="SeleccioneEspecialidad" class="form-label">Seleccione una Especialidad</label>
-                    <select id="SeleccioneEspecialidad" class="form-select">
-                        <option>Disabled select</option>
-                        
-                    </select>
-                </div>
-                <div class="mb-3" style="padding: 5% 0%">
-                    <label for="SeleccioneMedico" class="form-label">Seleccione un Médico</label>
-                    <select id="SeleccioneMedico" class="form-select">
-                        <option>Disabled select</option>
-                    </select>
-                </div>
-                
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
-            </main>
-        </div>
-        <div class="col-md-3 ml-sm-12 col-lg-3 col-xl-2 ">
-          <?php require_once('C:\xampp\htdocs\SIGMED\app\Views\Paciente\calendario\index.html'); ?>
-        </div>
-    </div>
 
 
-
-
-<!--codigo de opciones.php '''''NO VA AQUÍ''''''''-->
 
 </body>
 </html>
--- app/Views/Paciente/calendario/agendar.php
+++ app/Views/Paciente/calendario/agendar.php
@@ -12,11 +12,6 @@ html, body {
   margin: 0;
 }
 
-.full-height {
-  height: 100%;
-
-}
-
 </style>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
@@ -24,47 +19,45 @@ html, body {
 <body>
 
     
-<!--codigo de opciones.php '''''NO VA AQUÍ''''''''-->
-    
-<div class="row align-items-start" style=" height:100%">
-        <div class="col-md-3 ml-sm-12 col-lg-3 col-xl-2 " style="height:100%; background: #B9FFFF; ">
-            <div style="display: table; width:100%; height:8%;" >
-                <div  style="display: table-cell; background: #033FAC; width:100%; height:100%; color: #ffffff; text-align:center; vertical-align:middle">
-                    <h2 >Agendar Cita</h2>
-                </div>
+<div class="row" style=" height:100%">
+    <div class="col-md-3 ml-sm-12 col-lg-3 col-xl-2 px-0" style="height:100%; background: #B9FFFF; ">
+        <div style="display: table; width:100%;  height:8%;" >
+            <div  style="display: table-cell; background: #033FAC; width:100%; height:100%; color: #ffffff; text-align:center; vertical-align:middle">
+                <h2 >Agendar Cita</h2>
             </div>
-            <main role="main" class="col-md-11 ml-sm-11 col-lg-11 px-1">
-            <form>
-                <div class="mb-3" style="padding: 5% 0%">
-                    <label for="SeleccionePoliclinica" class="form-label">Seleccione una Policlínica</label>
-                    <!--<select class="form-control input-lg" id="nuevaCategoria" name="nuevaCategoria" required></select>-->
-                    <select id="SeleccionePoliclinica" class="form-select" name="SeleccionePoliclinica" required>  
-                        <option value="">Disabled select</option>               
-                        
-              </select>
-              </div>                 
-                <div class="mb-3" style="padding: 5% 0%">
-                    <label for="SeleccioneEspecialidad" class="form-label">Seleccione una Especialidad</label>
-                    <select id="SeleccioneEspecialidad" class="form-select">
-                        <option>Disabled select</option>
-                        
-                    </select>
-                </div>
-                <div class="mb-3" style="padding: 5% 0%">
-                    <label for="SeleccioneMedico" class="form-label">Seleccione un Médico</label>
-                    <select id="SeleccioneMedico" class="form-select">
-                        <option>Disabled select</option>
-                    </select>
-                </div>
-                
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
-            </main>
         </div>
-        <div class="col-md-9 ml-sm-12 col-lg-9 col-xl-10 ">
-          <?php require_once('index.html'); ?>
-        </div>
+        <main role="main" class="col-md-12 ml-sm-12 col-lg-12 px-1">
+        <form action="" method="post">
+            <div class="mb-3" style="padding: 5% 0%">
+                <label for="SeleccionePoliclinica" class="form-label">Seleccione una Policlínica</label>
+                <!--<select class="form-control input-lg" id="nuevaCategoria" name="nuevaCategoria" required></select>-->
+                <select id="SeleccionePoliclinica" class="form-select" name="SeleccionePoliclinica" required>  
+                    <option value="">Disabled select</option>               
+                    
+                </select>
+            </div>                 
+            <div class="mb-3" style="padding: 5% 0%">
+                <label for="SeleccioneEspecialidad" class="form-label">Seleccione una Especialidad</label>
+                <select id="SeleccioneEspecialidad" class="form-select" name="SeleccioneEspecialidad" required>
+                    <option>Disabled select</option>
+                    
+                </select>
+            </div>
+            <div class="mb-3" style="padding: 5% 0%">
+                <label for="SeleccioneMedico" class="form-label">Seleccione un Médico</label>
+                <select id="SeleccioneMedico" class="form-select" name="SeleccioneMedico" require>
+                    <option>Disabled select</option>
+                </select>
+            </div>
+            
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+        </main>
     </div>
-<!--codigo de opciones.php '''''NO VA AQUÍ''''''''-->
+    <div class="col-md-9 ml-sm-12 col-lg-9 col-xl-10 px-0">
+        <?php require_once('index.html'); ?>
+    </div>
+</div>
+
 </body>
 </html>
